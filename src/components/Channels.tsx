892f49d8d72f4f06718170e0f5a9a66bbf8e15c2
Viacheslav Horodovych <viacheslav.horodovych@collibra.com>
2019-11-10 22:29:29 +0100

    More sidebar styling

--- src/components/Channels.tsx
+++ src/components/Channels.tsx
@@ -5,17 +5,28 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 import {
   CHANNELS_QUERY,
 } from '../GQLQuery';
 
-type TParams = {
-  id: string
-};
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    marginRight: '1rem',
+    backgroundColor: '#B1AAC4',
+    color: '#48435B'
+  },
+  channelName: {
+    color: '#8A7EB1',
+    fontWeight: 'normal'
+  }
+}));
 
 export const Channels: React.FC = () => {
   const { loading, error, data } = useQuery(CHANNELS_QUERY);
+  const classes = useStyles();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -27,8 +38,10 @@ export const Channels: React.FC = () => {
         data &&
         data.channels.map(({ id, name }: { id: string, name: string }) => (
           <ListItem button component={Link} to={`/channel/${id}`}>
-            <Avatar>{name[0]}</Avatar>
-            <ListItemText primary={name} />
+            <Avatar className={classes.avatar}>{name[0]}</Avatar>
+            <ListItemText primary={
+              <Typography variant="h5" className={classes.channelName}>{name}</Typography>
+            } />
           </ListItem>
         ))
       }
